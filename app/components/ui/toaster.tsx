9c70ce037f87073ef6aae5bd3140b0c9f0a5751a
Abubakar Azmi <abubakarshaikh888@gmail.com>
2026-02-09 19:45:59 +0530

    update

--- app/components/ui/toaster.tsx
+++ app/components/ui/toaster.tsx
@@ -1,4 +1,4 @@
-import { useToast } from "@/hooks/use-toast";
+import { useToast } from "@/app/hooks/use-toast";
 import { Toast, ToastClose, ToastDescription, ToastProvider, ToastTitle, ToastViewport } from "@/app/components/ui/toast";
 
 export function Toaster() {
