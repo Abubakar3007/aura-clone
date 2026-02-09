aea09093cd67dc1538091d135698e07505e7d5f6
Abubakar Azmi <abubakarshaikh888@gmail.com>
2026-02-09 19:32:38 +0530

    update

--- app/components/ui/sidebar.tsx
+++ app/components/ui/sidebar.tsx
@@ -5,7 +5,7 @@ import { PanelLeft } from "lucide-react";
 
 import { useIsMobile } from "@/hooks/use-mobile";
 import { cn } from "@/app/components/lib/utils";
-import { Button } from "@/components/ui/button";
+import { Button } from "@/app/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Separator } from "@/components/ui/separator";
 import { Sheet, SheetContent } from "@/components/ui/sheet";
